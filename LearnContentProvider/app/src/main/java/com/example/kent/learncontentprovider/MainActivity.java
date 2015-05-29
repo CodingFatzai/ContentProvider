f003a0a7ed38c9b4509df56656309194fb87ad0c
Jeffchao831 <j7173381@gmail.com>
2015-05-29 22:42:08 +0800

    Comment a GitTesting

--- LearnContentProvider/app/src/main/java/com/example/kent/learncontentprovider/MainActivity.java
+++ LearnContentProvider/app/src/main/java/com/example/kent/learncontentprovider/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //GitTesting
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainactivity);
 
